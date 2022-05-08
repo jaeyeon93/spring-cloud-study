352b2508c004dd9354a711c3e1bc13a09a988d53
jaeyeon kim <foodsksms@gmail.com>
2022-05-08 22:55:20 +0900

    tmp

---
+++ discoveryservice/src/test/java/me/jimmy/discoveryservice/DiscoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jimmy.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
