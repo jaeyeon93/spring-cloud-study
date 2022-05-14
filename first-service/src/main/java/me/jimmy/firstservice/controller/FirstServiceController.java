67516c12ae103d4e3f000f66a43054c2fd961df6
jaeyeon kim <foodsksms@gmail.com>
2022-05-14 16:01:45 +0900

    api gateway에 LB적용

--- first-service/src/main/java/me/jimmy/firstservice/controller/FirstServiceController.java
+++ first-service/src/main/java/me/jimmy/firstservice/controller/FirstServiceController.java
@@ -1,13 +1,24 @@
 package me.jimmy.firstservice.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/first-service")
+@Slf4j
 public class FirstServiceController {
+    Environment env;
+
+    public FirstServiceController(Environment env) {
+        this.env = env;
+    }
+
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to First Service";
@@ -20,7 +31,8 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi, This is message of First Service";
+    public String check(HttpServletRequest request) {
+        log.info("Server port={}", request.getServerPort());
+        return String.format("Hi, This is message of First Service on PORT %s", env.getProperty("local.server.port"));
     }
 }
