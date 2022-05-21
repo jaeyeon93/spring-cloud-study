3424834d0d5087bb78e3fb8be2ec813f4c30c6c0
JaeyeonKIM <foodsksms@gmail.com>
2022-05-21 16:03:48 +0900

    Add Feign Logger.Level

--- userservice/src/main/java/me/jimmy/userservice/config/WebConfig.java
+++ userservice/src/main/java/me/jimmy/userservice/config/WebConfig.java
@@ -1,5 +1,6 @@
 package me.jimmy.userservice.config;
 
+import feign.Logger;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,5 +19,10 @@ public class WebConfig {
     @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
     }
 }
