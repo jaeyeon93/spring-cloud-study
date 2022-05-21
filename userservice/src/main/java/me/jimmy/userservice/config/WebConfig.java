12710bb99afb132331fc3b967b0552b9f6db7b60
JaeyeonKIM <foodsksms@gmail.com>
2022-05-22 01:07:55 +0900

    Feign Error Decoder apply

--- userservice/src/main/java/me/jimmy/userservice/config/WebConfig.java
+++ userservice/src/main/java/me/jimmy/userservice/config/WebConfig.java
@@ -1,6 +1,8 @@
 package me.jimmy.userservice.config;
 
 import feign.Logger;
+import feign.codec.ErrorDecoder;
+import me.jimmy.userservice.error.FeignErrorDecoder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,4 +27,9 @@ public class WebConfig {
     public Logger.Level feignLoggerLevel() {
         return Logger.Level.FULL;
     }
+//
+//    @Bean
+//    public ErrorDecoder feignErrorDecoder() {
+//        return new FeignErrorDecoder();
+//    }
 }
