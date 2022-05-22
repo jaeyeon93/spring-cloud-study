4da1e6b0aacac85fcebc87e31fcc92570d2b5396
JaeyeonKIM <foodsksms@gmail.com>
2022-05-22 17:12:16 +0900

    Revert "Feign Error Decoder apply"
    This reverts commit 12710bb99afb132331fc3b967b0552b9f6db7b60.

--- userservice/src/main/java/me/jimmy/userservice/config/WebConfig.java
+++ userservice/src/main/java/me/jimmy/userservice/config/WebConfig.java
@@ -1,8 +1,6 @@
 package me.jimmy.userservice.config;
 
 import feign.Logger;
-import feign.codec.ErrorDecoder;
-import me.jimmy.userservice.error.FeignErrorDecoder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,9 +25,4 @@ public class WebConfig {
     public Logger.Level feignLoggerLevel() {
         return Logger.Level.FULL;
     }
-//
-//    @Bean
-//    public ErrorDecoder feignErrorDecoder() {
-//        return new FeignErrorDecoder();
-//    }
 }
